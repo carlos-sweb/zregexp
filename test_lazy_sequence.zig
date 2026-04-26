b9743229a4e27ee90530aff3a5301faaaa7f0e00
carlos-sweb <c4rl0sill3sc4@protonmail.com>
2026-04-26 00:04:54 -0400

    move 0.15 to 0.16

--- test_lazy_sequence.zig
+++ test_lazy_sequence.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const regex = @import("src/regex.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa : std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
